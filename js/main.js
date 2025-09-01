cbcedd896b4538c6c23c4142ec1ff6879cd91645
Goodvick <teplicov23@gmail.com>
2025-09-01 18:05:30 +0300

    шрифты теперь работают на всех браузерах, добавлен слайдер на главное изображение

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+   const slider = $('.main__slider')
+   const items = $('.main__item-slider')
+   const allSlides = items.length
+
+   let sliderAutoPlay
+
+   const setupSlider = {
+      currentSlide: 0,
+      autoPlay: true,
+      interval: 4000,
+   }
+
+   function switchSlide(currentIndex) {
+      if (currentIndex >= allSlides) {
+         currentIndex = 0;
+      } else if (currentIndex < 0) {
+         currentIndex = allSlides - 1;
+      }
+      setupSlider.currentSlide = currentIndex;
+      slider.css('transform', `translateX(-${currentIndex * 100}%)`);
+   }
+   function nextSlide() {
+      switchSlide(setupSlider.currentSlide + 1);
+   }
+   function startAutoPlay() {
+      if (setupSlider.autoPlay) {
+         sliderAutoPlay = setInterval(nextSlide, setupSlider.interval);
+      }
+   }
+   function stopAutoPlay() {
+      clearInterval(sliderAutoPlay);
+   }
+
+   $('.main__slider').hover(
+      function () {
+         stopAutoPlay();
+      },
+      function () {
+         startAutoPlay();
+      }
+   );
+
+   startAutoPlay();
+})
